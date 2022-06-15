355f9aa1279c1b730e28678016d7bb00a4e5feae
Michael Wheeler <mpwheel@umich.edu>
2022-06-15 13:55:04 -0400

    Specify line length of 88 characters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 const { createConfig } = require('@edx/frontend-build');
 
-module.exports = createConfig('eslint');
+module.exports = createConfig('eslint', {
+  rules: {
+    'max-len': ['error', 88, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }]
+  },
+});
