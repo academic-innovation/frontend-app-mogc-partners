127b446d5d5dadaeddb75628ede34d82d528d533
Michael Wheeler <mpwheel@umich.edu>
2022-07-21 12:10:52 -0400

    Refactor entity access into custom hooks

--- src/features/members/CatalogMemberCount.jsx
+++ src/features/members/CatalogMemberCount.jsx
@@ -1,21 +1,10 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { fetchMembers, selectAllMembers } from './membersSlice';
+import useMembers from './useMembers';
 
 export default function CatalogMemberCount({ catalog }) {
-  const dispatch = useDispatch();
-  const members = useSelector(selectAllMembers);
-  const membersStatus = useSelector(state => state.members.status);
-
-  useEffect(() => {
-    if (membersStatus === 'idle') {
-      dispatch(fetchMembers());
-    }
-  }, [membersStatus, dispatch]);
-
+  const [members] = useMembers();
   const count = members.filter(member => member.catalog === catalog).length;
-
   return <span>{count} member{count === 1 ? '' : 's'}.</span>;
 }
 
--- src/features/offerings/CatalogOfferingCount.jsx
+++ src/features/offerings/CatalogOfferingCount.jsx
@@ -1,21 +1,10 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { fetchOfferings, selectAllOfferings } from './offeringsSlice';
+import useOfferings from './useOfferings';
 
 export default function CatalogOfferingCount({ catalog }) {
-  const dispatch = useDispatch();
-  const offerings = useSelector(selectAllOfferings);
-  const offeringsStatus = useSelector(state => state.offerings.status);
-
-  useEffect(() => {
-    if (offeringsStatus === 'idle') {
-      dispatch(fetchOfferings());
-    }
-  }, [offeringsStatus, dispatch]);
-
+  const [offerings] = useOfferings();
   const count = offerings.filter(offering => offering.catalog === catalog).length;
-
   return <span>{count} course offering{count === 1 ? '' : 's'}.</span>;
 }
 
