349b8f006ee838eb8480a1ac1bac6173656bdb9c
Michael Wheeler <mpwheel@umich.edu>
2022-07-21 13:27:05 -0400

    Prevent multiple requests for the same data

--- src/features/offerings/offeringsSlice.js
+++ src/features/offerings/offeringsSlice.js
@@ -12,6 +12,7 @@ const offeringsAdapter = createEntityAdapter({
 
 const initialState = offeringsAdapter.getInitialState({
   status: 'idle',
+  currentRequestId: undefined,
   error: null,
 });
 
@@ -26,7 +27,11 @@ export async function enrollInOffering(offeringId) {
 
 export const fetchOfferings = createAsyncThunk(
   'offerings/fetchOfferings',
-  async () => {
+  async (args, { getState, requestId }) => {
+    const { currentRequestId, status } = getState().offerings;
+    if (status !== 'loading' || requestId !== currentRequestId) {
+      return undefined;
+    }
     const client = getAuthenticatedHttpClient();
     const baseUrl = getConfig().LMS_BASE_URL;
     const response = await client.get(`${baseUrl}/api/partnerships/v0/offerings/`);
@@ -54,16 +59,31 @@ const offeringsSlice = createSlice({
   reducers: {},
   extraReducers(builder) {
     builder
-      .addCase(fetchOfferings.pending, (state) => {
-        state.status = 'loading';
+      .addCase(fetchOfferings.pending, (state, action) => {
+        if (state.status === 'idle') {
+          state.status = 'loading';
+          state.currentRequestId = action.meta.requestId;
+        }
       })
       .addCase(fetchOfferings.fulfilled, (state, action) => {
-        state.status = 'success';
-        offeringsAdapter.upsertMany(state, action.payload);
+        const { requestId } = action.meta;
+        if (
+          state.status === 'loading'
+          && state.currentRequestId === requestId
+        ) {
+          state.status = 'success';
+          offeringsAdapter.upsertMany(state, action.payload);
+        }
       })
       .addCase(fetchOfferings.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.error.message;
+        const { requestId } = action.meta;
+        if (
+          state.status === 'loading'
+          && state.currentRequestId === requestId
+        ) {
+          state.status = 'failed';
+          state.error = action.error.message;
+        }
       })
       .addCase(addOffering.fulfilled, offeringsAdapter.addOne);
   },
