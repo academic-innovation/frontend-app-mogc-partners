b68b491685a257206c47b3ab0dd4d755438386bd
Michael Wheeler <mpwheel@umich.edu>
2023-03-01 10:44:53 -0500

    Add organizational stat cards

---
+++ src/features/enrollments/EnrollmentStatCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import StatCard from '../partners/StatCard';
+import useRecords from './useRecords';
+
+export default function EnrollmentStatsCard({ partner, onlyComplete }) {
+  const singleUnit = onlyComplete ? 'Completion' : 'Enrollment';
+  const pluralUnit = onlyComplete ? 'Completions' : 'Enrollments';
+  const [enrollments, status] = useRecords();
+  if (status !== 'success') {
+    return <StatCard value="--" unit="Enrollments" />;
+  }
+  const enrollmentTotal = enrollments.filter(enrollment => {
+    if (enrollment.offering.partner !== partner) {
+      return false;
+    }
+    return onlyComplete ? enrollment.isComplete : true;
+  }).length;
+  const unit = enrollmentTotal === 1 ? singleUnit : pluralUnit;
+
+  return <StatCard value={enrollmentTotal} unit={unit} />;
+}
+
+EnrollmentStatsCard.propTypes = {
+  partner: PropTypes.string.isRequired,
+  onlyComplete: PropTypes.bool,
+};
+
+EnrollmentStatsCard.defaultProps = {
+  onlyComplete: false,
+};
---
+++ src/features/members/MemberStatCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import StatCard from '../partners/StatCard';
+import useMembers from './useMembers';
+
+export default function MemberStatsCard({ partner }) {
+  const [members, status] = useMembers();
+  if (status !== 'success') {
+    return <StatCard value="--" unit="Members" />;
+  }
+  const partnerMembers = members.filter(member => member.partner === partner);
+  const partnerMemberEmails = new Set(partnerMembers.map(member => member.email));
+  const memberTotal = partnerMemberEmails.size;
+  const unit = memberTotal === 1 ? 'Member' : 'Members';
+
+  return <StatCard value={partnerMemberEmails.size} unit={unit} />;
+}
+
+MemberStatsCard.propTypes = {
+  partner: PropTypes.string.isRequired,
+};
