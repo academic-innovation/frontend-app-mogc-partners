bc5385bd2c62268ec350ba2697d23ca01a0acf29
Edward Cochran <ecochra@gmail.com>
2023-11-21 15:35:00 -0500

    Modified props and conditional rendering

--- src/features/partners/StatCard.jsx
+++ src/features/partners/StatCard.jsx
@@ -2,27 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from '@edx/paragon';
 
-export default function StatCard({ value, unit, isTotal }) {
-  const totalText = <span className="lead d-block">Total</span>;
+export default function StatCard({ value, unit, secondary }) {
   return (
     <Card>
       <Card.Section className="text-center">
         <span className="display-2 d-block">{ value }</span>
-        {
-          isTotal && totalText
-        }
         <span className="lead d-block">{ unit }</span>
+        {
+          secondary && <span className="lead d-block">{secondary}</span>
+        }
       </Card.Section>
     </Card>
   );
 }
 
-StatCard.defaultProps = {
-  isTotal: false,
-};
-
 StatCard.propTypes = {
   value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
   unit: PropTypes.string.isRequired,
-  isTotal: PropTypes.bool,
+  secondary: PropTypes.string,
+};
+
+StatCard.defaultProps = {
+  secondary: '',
 };
