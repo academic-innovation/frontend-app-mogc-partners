203c44bec21c49aee9ced2e1596d8bc11d703937
Edward Cochran <ecochra@gmail.com>
2023-11-21 11:10:44 -0500

    Removed unused const

--- src/features/partners/PartnerStats.jsx
+++ src/features/partners/PartnerStats.jsx
@@ -15,7 +15,6 @@ export default function PartnerStats() {
 
   const courseCount = partner?.offerings.length;
   const courseUnit = courseCount === 1 ? 'Course' : 'Courses';
-  const isTotal = true;
 
   return (
     <>
