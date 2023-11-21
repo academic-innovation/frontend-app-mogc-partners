e5f9006e367869e1b463788430f9f6a3e03703cf
Edward Cochran <ecochra@gmail.com>
2023-11-21 13:24:56 -0500

    Added dynamic button styles

--- src/features/partners/ManagementToolbar.jsx
+++ src/features/partners/ManagementToolbar.jsx
@@ -4,23 +4,27 @@ import { ButtonToolbar, ButtonGroup, Button } from '@edx/paragon';
 import { NavLink } from 'react-router-dom';
 
 export default function ManagementToolbar({ partner }) {
+  const url = new URL(window.location);
+  const isInsights = url.pathname.includes("insights");
   return (
     <div className="row">
       <div className="col col-9">
         <ButtonToolbar>
           <ButtonGroup>
-            <Button>
-              <NavLink to={`/${partner}/admin`} style={{ color: 'white' }}>Admin</NavLink>
+            <Button style={{ background: !isInsights ? 'red' : 'white' }}>
+              <NavLink to={`/${partner}/admin`} style={{ color: !isInsights ? 'red' : 'white' }}>
+                Cohorts
+              </NavLink>
             </Button>
-            <Button>
-              <NavLink to={`/${partner}/admin/insights`} style={{ color: 'red' }}>Insights</NavLink>
+            <Button style={{ background: isInsights ? 'red' : 'white' }}>
+              <NavLink to={`/${partner}/admin/insights`} style={{ color: isInsights ? 'red' : 'white' }}>Insights</NavLink>
             </Button>
           </ButtonGroup>
         </ButtonToolbar>
       </div>
       <div className="col col-3">
         <Button>
-          <NavLink to={`/${partner}`}>Manage</NavLink>
+          <NavLink to={`/${partner}`}>Preview As Learner</NavLink>
         </Button>
       </div>
     </div>
