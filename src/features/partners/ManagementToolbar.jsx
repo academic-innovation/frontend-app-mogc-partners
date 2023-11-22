bba6910bf26de2da20d9151b5e40132ab303cfe2
Edward Cochran <ecochra@gmail.com>
2023-11-22 11:23:56 -0500

    Modified prop for better class selection, and removed unnecessary code

--- src/features/partners/ManagementToolbar.jsx
+++ src/features/partners/ManagementToolbar.jsx
@@ -3,21 +3,19 @@ import PropTypes from 'prop-types';
 import { ButtonToolbar, ButtonGroup, Button } from '@edx/paragon';
 import { NavLink } from 'react-router-dom';
 
-export default function ManagementToolbar({ partner }) {
-  const url = new URL(window.location);
-  const isInsights = url.pathname.includes('insights');
+export default function ManagementToolbar({ partner, selectedTab }) {
   return (
     <div className="row">
       <div className="col col-9">
         <ButtonToolbar>
           <ButtonGroup>
-            <Button style={{ background: !isInsights ? 'red' : 'white' }}>
-              <NavLink to={`/${partner}/admin`} style={{ color: !isInsights ? 'red' : 'white' }}>
+            <Button style={{ background: selectedTab === 'cohorts' ? 'red' : 'white' }}>
+              <NavLink to={`/${partner}/admin`} style={{ color: selectedTab === 'cohorts' ? 'white' : 'red' }}>
                 Cohorts
               </NavLink>
             </Button>
-            <Button style={{ background: isInsights ? 'red' : 'white' }}>
-              <NavLink to={`/${partner}/admin/insights`} style={{ color: isInsights ? 'red' : 'white' }}>Insights</NavLink>
+            <Button style={{ background: selectedTab === 'insights' ? 'red' : 'white' }}>
+              <NavLink to={`/${partner}/admin/insights`} style={{ color: selectedTab === 'insights' ? 'white' : 'red' }}>Insights</NavLink>
             </Button>
           </ButtonGroup>
         </ButtonToolbar>
@@ -31,6 +29,11 @@ export default function ManagementToolbar({ partner }) {
   );
 }
 
+ManagementToolbar.defaultProps = {
+  selectedTab: null,
+};
+
 ManagementToolbar.propTypes = {
   partner: PropTypes.string.isRequired,
+  selectedTab: PropTypes.string,
 };
