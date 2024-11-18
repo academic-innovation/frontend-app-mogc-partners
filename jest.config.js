e1b1546188280cf992a87480150990adcd361581
Daniel Merritt <djmerrit@umich.edu>
2024-11-18 16:38:18 -0500

    Expand jest transform config to @openedx/paragon

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line
 const { createConfig } = require('@edx/frontend-build');
 
-module.exports = createConfig('jest', {
-  // setupFilesAfterEnv is used after the jest environment has been loaded.  In general this is what you want.  
+const config = createConfig('jest', {
+  // setupFilesAfterEnv is used after the jest environment has been loaded.  In general this is what you want.
   // If you want to add config BEFORE jest loads, use setupFiles instead.  
   setupFilesAfterEnv: [
     '<rootDir>/src/setupTest.js',
@@ -12,3 +12,13 @@ module.exports = createConfig('jest', {
     'src/i18n',
   ],
 });
+
+// The default config in @edx/frontend-build ignores all modules that are not
+// @edx, so we cannot add another pattern. We have to replace the existing
+// pattern with one that handles both @edx and @openedx.
+delete config['transformIgnorePatterns'];
+config['transformIgnorePatterns'] = [
+  '<rootDir>/node_modules/(?!@edx/|@openedx/paragon)',
+]
+
+module.exports = config;
